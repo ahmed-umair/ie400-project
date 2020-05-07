dd56d992ae59a4417ee9481bbf460aa86f152a48
Muhammad Umair Ahmed <umair.ahmed@piworks.net>
2020-05-07 23:56:13 +0300

    Added comments explaining my approach

--- data-generator.py
+++ data-generator.py
@@ -17,30 +17,44 @@ def generate_data(n: int):
     student_times_Xi = np.round(student_times_Xi)
     print(student_times_Xi)
 
-    # Enumerate the applicable N values up to N=n
+    # Enumerate the applicable N values up to N=n i.e. N=5,10,15...n
     N_set = range(5, n + 5, 5)
     print(f"\n {N_set}")
 
-    N = []
+    # Create a list to hold the final travel_time matrices for N=5,10,15,... up to the function argument n 
+    # (which should be 75 at max i.e. 16 matrices in total)
+    travel_time_matrices_list = []
     for i in N_set:
         print(f"\nCurrent N: {i}")
+        # Enumerate num of variants from 1 to (i)(ln(i)) where i is a member of N=5,10,15,...
         variants = range(1, math.ceil(i * math.log(i)))
         print(f"\t {variants}")
-        
-        ith_travel_time_matrix = np.zeros((i,i))
+
+        # Initialize an empty matrix with shape (i,i)
+        ith_travel_time_matrix = np.zeros((i, i))
+
+        # Enumerate (i)(ln(i)) and get their average
         for variant_num in variants:
-            current_variant =   (np.round((valid_travel_time_range[1] - valid_travel_time_range[0]
-                                            ) * np.random.rand(i,i) + valid_travel_time_range[0]))
+            # Generate a variant
+            current_variant = (np.round((valid_travel_time_range[1] - valid_travel_time_range[0]
+                                         ) * np.random.rand(i, i) + valid_travel_time_range[0]))
             print(current_variant)
+            # Add it elementwise to accumulator matrix
             ith_travel_time_matrix = ith_travel_time_matrix + current_variant
-        
+
         print(variant_num)
+        # divide by the i(ln(i)) to get element-wise average matrix
         ith_travel_time_matrix = np.round(ith_travel_time_matrix/variant_num)
         print(ith_travel_time_matrix)
-        N.append(ith_travel_time_matrix)
-    
+
+        # Append averaged travel_time matrix to list of travel_time matricess
+        travel_time_matrices_list.append(ith_travel_time_matrix)
+
+    # Print all of the calculated matrices
     for i in range(len(N_set)):
-        print(f"\n\n THIS IS THE FIRST TRAVEL_TIME MATRIX FOR N = {N_set[i]} \n {N[i]}")
+        print(
+            f"\n\n THIS IS THE FIRST TRAVEL_TIME MATRIX FOR N = {N_set[i]} \n {travel_time_matrices_list[i]}")
+
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
